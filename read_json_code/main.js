e6471415069fc6ad005dfabc80002c18986e1fb7
John Angeles <jpangeles16@gmail.com>
2019-08-08 15:02:29 -0700

    First commit in case computer crashes

---
+++ read_json_code/main.js
@@ -0,0 +1,18 @@
+console.log("Hello, world!");
+
+// Fetches the button from the document
+var btn = document.getElementById("demo");
+
+// btn.addEventListener("click", function() {
+// 	// Fetches the file online
+// 	var ourRequest = new XMLHttpRequest();
+// 	ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+
+// 	// Logs the data onto the console
+// 	ourRequest.onload = function() {
+// 		var ourData = JSON.parse(ourRequest.responseText);
+// 		console.log(ourData[0]);
+
+// 		ourRequest.send();
+// 	}
+// });
