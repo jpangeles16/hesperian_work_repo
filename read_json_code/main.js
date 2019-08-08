df552edd3409aedcff77bd3474cc6e9985026b0a
John Angeles <jpangeles16@gmail.com>
2019-08-08 16:42:22 -0700

    Before pulling

--- read_json_code/main.js
+++ read_json_code/main.js
@@ -3,16 +3,29 @@ console.log("Hello, world!");
 // Fetches the button from the document
 var btn = document.getElementById("demo");
 
-// btn.addEventListener("click", function() {
-// 	// Fetches the file online
-// 	var ourRequest = new XMLHttpRequest();
-// 	ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+var animalContainer = document.getElementById("animal-info");
 
-// 	// Logs the data onto the console
-// 	ourRequest.onload = function() {
-// 		var ourData = JSON.parse(ourRequest.responseText);
-// 		console.log(ourData[0]);
+btn.addEventListener("click", function() {
+	// Fetches the file online
+	var ourRequest = new XMLHttpRequest();
+	ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
 
-// 		ourRequest.send();
-// 	}
-// });
+	// Logs the data onto the console
+	ourRequest.onload = function() {
+		var ourData = JSON.parse(ourRequest.responseText);
+		renderHTML(ourData);
+	}
+
+	ourRequest.send();
+});
+
+// Function is to have a sole job to log onto HTML.
+function renderHTML(data) {
+	var htmlString = "";
+
+	for (i = 0; i < data.length; i ++) {
+		htmpString += "<p>" + data[i].name + " is a " + data[i].species + ".</p>";
+	}
+
+	animalContainer.insertAdjacentHTML('beforeend', htmlString);
+}
