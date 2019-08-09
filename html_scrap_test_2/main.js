b38a96ecc2a7b12a865a387e6289c865bdedd26c
John Angeles <jpangeles16@gmail.com>
2019-08-09 15:02:15 -0700

    Answers

--- html_scrap_test_2/main.js
+++ html_scrap_test_2/main.js
@@ -33,11 +33,76 @@ from a parsed file, displays the results in a human-readable fashion.
 */
 function displayQuestionsAndAnswers(questionsList) {
 	var i;
-
 	// For loop that basically iterates through the list and prints out what the user responded.
 	for (i = 0; i < questionsList.length; i ++) {
-		console.log("Question " + i + ":");
-		console.log(questionsList[i]);
-		console.log("-------------------");
+		//Every index into questionsList will be a question object.
+		processQuestionAndAnswerObject(questionsList[i]);
 	}
 }
+
+/* To be called from displayQuestionsAndAnswers. It takes in an object
+that has a question and a set of answers.
+*/
+function processQuestionAndAnswerObject(questionAndAnswerObject) {
+	var question = questionAndAnswerObject.question;
+	switch (question) {
+		case "qMostImportant":
+			processQMostImportant(questionAndAnswerObject);
+			return;
+		case "qRiskFactors":
+			console.log("Risk factors");
+			return;
+		case "qPeriodProblems":
+			console.log("Period problems question");
+			return;
+		case "qWhichHaveYouUsed":
+			console.log("Which have I used question");
+			return;
+		case "qOtherMedications":
+			console.log("Other medications question");
+			return;
+		case "qOtherHealthIssues":
+			console.log("qOtherHealthIssues");
+			return;
+		case "qWhenHaveChildren":
+			console.log("qWhenHaveChildren");
+			return;
+		case "qOtherQuestions":
+			console.log("qOtherQuestions");
+			return;
+		case "qMethodSpecific":
+			console.log("Last question I think");
+			return;
+		default:
+			console.log("Nothing happened!");
+			return;
+	}
+}
+
+/* Mini helper function that, when given an answer, returns either "YES" or "NO"
+depending on what the user has answered.
+*/
+function getAnswer(answer) {
+	var lastChar = answer[answer.length - 1]
+	if (answer[answer.length - 1] == "o") {
+		return "NO";
+	} else {
+		return "YES";
+	}
+}
+
+
+/* Process question 1 assuming we pass in questionObject as Q1.
+*/
+function processQMostImportant(questionObject) {
+	var answers = questionObject.answers;
+	for (var prop in answers) {
+		var answer = getUserResponse(answers[prop]);
+		// userAnswer is the user's answer
+		console.log(answer);
+	}
+}
+
+
+
+
